0c9b9df4c0f805dd789af716db9bbcfd4af85575
Stefan Probst <stefanprobst@posteo.de>
2024-09-30 13:41:47 +0200

    chore: wip

--- eslint.config.js
+++ eslint.config.js
@@ -4,15 +4,33 @@ import baseConfig from "@acdh-oeaw/eslint-config";
 import astroConfig from "@acdh-oeaw/eslint-config-astro";
 import playwrightConfig from "@acdh-oeaw/eslint-config-playwright";
 import reactConfig from "@acdh-oeaw/eslint-config-react";
+import solidJsConfig from "@acdh-oeaw/eslint-config-solid";
 import tailwindcssConfig from "@acdh-oeaw/eslint-config-tailwindcss";
 import gitignore from "eslint-config-flat-gitignore";
+
+const reactFiles = [
+	"keystatic.config.@(ts|tsx)",
+	"**/content/**/*.@(ts|tsx)",
+	"**/keystatic/**/*.@(ts|tsx)",
+];
 
 /** @type {Config} */
 const config = [
 	gitignore({ strict: false }),
 	...baseConfig,
 	...astroConfig,
-	...reactConfig,
+	...reactConfig.map((config) => {
+		return {
+			files: reactFiles,
+			...config,
+		};
+	}),
+	...solidJsConfig.map((config) => {
+		return {
+			ignores: reactFiles,
+			...config,
+		};
+	}),
 	...tailwindcssConfig,
 	...playwrightConfig,
 	{
@@ -20,8 +38,13 @@ const config = [
 			"arrow-body-style": ["error", "always"],
 			"prefer-arrow-callback": ["error", { allowNamedFunctions: true }],
 			// "@typescript-eslint/explicit-module-boundary-types": "error",
-			"@typescript-eslint/require-array-sort-compare": "error",
+			// "@typescript-eslint/require-array-sort-compare": "error",
 			// "@typescript-eslint/strict-boolean-expressions": "error",
+		},
+	},
+	{
+		files: reactFiles,
+		rules: {
 			"react/jsx-sort-props": ["error", { reservedFirst: true }],
 		},
 	},
